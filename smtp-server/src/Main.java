e4370316fb8b6a8114995224fd6220aa65c08f2a
Raúl Martínez (RME) <i-rme@users.noreply.github.com>
2020-03-05 00:42:39 +0100

    Implemented some functions to send, receive and parse messages
    Implemented some functions to send, receive and parse messages.
    
    	/*
    	 * Comentarios
    	 * 
    	 * Web para probar expresiones regulares con java: https://www.freeformatter.com/java-regex-tester.html
    	 * 
    	 * Funciones
    	 * 
    	 * sendServerMessage: Forma un mensaje del servidor con un codigo y una string, añade <CRLF> al final
    	 * waitClientMessage: Pide una string de respuesta y la devuelve
    	 * waitClientMessageRegex: Pide una string de respuesta, la parsea segun la expresion regular y devuelve lo importante
    	 * waitMultilineClientMessage: Pide varias string, aun sin terminar
    	 */

--- smtp-server/src/Main.java
+++ smtp-server/src/Main.java
@@ -1,35 +1,41 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		String server_name = "server.local";
-		
-		System.out.print("220 " + server_name + " Service Ready\r\n");
-		
-        Scanner scanner = new Scanner(System. in);
-        String inputString = scanner. nextLine();
-        //System.out.println("String read from console is : \n"+inputString);
-        // HELO client.example.com\r\n
-        
-		System.out.print("250 Request mail action okay, completed\r\n");
+	/*
+	 * Comentarios
+	 * 
+	 * Web para probar expresiones regulares con java: https://www.freeformatter.com/java-regex-tester.html
+	 * 
+	 * Funciones
+	 * 
+	 * sendServerMessage: Forma un mensaje del servidor con un codigo y una string, añade <CRLF> al final
+	 * waitClientMessage: Pide una string de respuesta y la devuelve
+	 * waitClientMessageRegex: Pide una string de respuesta, la parsea segun la expresion regular y devuelve lo importante
+	 * waitMultilineClientMessage: Pide varias string, aun sin terminar
+	 */
+	
+	private static Scanner scanner;
 
-        inputString = scanner.nextLine();
-        //System.out.println("String read from console is : \n"+inputString);
-        // MAIL FROM: <user@example.com>\r\n
+	public static void main(String[] args) {
+
+		String SERVER_NAME = "server.local";
+		
+		
+		sendServerMessage(220, SERVER_NAME + " Service Ready");
+		waitClientMessageRegex("(HELO) (([a-zA-Z0-9]+)(\\.([a-zA-Z0-9]+))*)");	// HELO client.example.com\r\n
         
-        System.out.print("250 OK\r\n");
         
-        inputString = scanner.nextLine();
-        //System.out.println("String read from console is : \n"+inputString);
-        // DATA\r\n
+		sendServerMessage(250, "Request mail action okay, completed");
+		waitClientMessageRegex("(MAIL FROM:) <([a-zA-Z0-9]+@(([a-zA-Z0-9]+)(.([a-zA-Z0-9]+))+))>");	// MAIL FROM: <user@example.com>\r\n
         
-        System.out.print("354 Start mail input; end with <CRLF>.\r\n");
-        
-        inputString = scanner.nextLine();
-        //System.out.println("String read from console is : \n"+inputString);
+		sendServerMessage(250, "OK");
+		waitClientMessageRegex("((DATA))");	// DATA\r\n
+              
+        sendServerMessage(354, "Start mail input; end with <CRLF>.<CRLF>");
+        waitMultilineClientMessage();
         //Subject:<Subject>\r\n
         //From:<email@domain.com>\r\n
         //To:<user1@server.local>,<user2@server.local>\r\n
@@ -37,14 +43,52 @@ public class Main {
         // Esto es un mensaje de prueba, /nDos lineas..
         // \r\n.\r\n.
         
-        System.out.print("250 OK\r\n");
+		sendServerMessage(250, "OK");
+		waitClientMessageRegex("((QUIT))");	// QUIT\r\n
         
-        inputString = scanner.nextLine();
-        //System.out.println("String read from console is : \n"+inputString);
-        // QUIT\r\n
-        
-        System.out.print("221 " + server_name + " Service closing transmission channel\r\n");
+		sendServerMessage(221, SERVER_NAME + " Service closing transmission channel");
         
 	}
+	
+	public static void sendServerMessage(int code, String comment){
+        String message = code + " " + comment + "\r\n";
+        
+        System.out.print(message);
+	}
+	
+	public static String waitClientMessage(){
+        scanner = new Scanner(System. in);
+        String message = scanner.nextLine();
+        
+        return message;
+	}
+	
+	public static String waitClientMessageRegex(String pattern){
+        scanner = new Scanner(System. in);
+        String message = scanner.nextLine();
+        
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(message);
 
+        if (m.matches()) {
+        	return m.group(2);	// 0: whole matched expression, 1: first expression in brackets, 2: second exp, ...
+        }else {
+        	System.out.println("Error: Incorrect message for client");
+        	return null;
+        }
+        
+	}
+	
+	public static String waitMultilineClientMessage(){
+		scanner = new Scanner(System. in);
+		String message = "";
+		
+		while(scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			message = message.concat(line + "\r\n");
+			if( message.contains("\r\n.\r\n") ) break;
+		}
+
+        return message;
+	}
 }
