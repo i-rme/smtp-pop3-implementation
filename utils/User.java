e68dc925953b9f835817758d0876279ac300b0e2
Marutho <marutho97@gmail.com>
2020-03-25 15:50:02 +0100

    Javi
    Javi

--- utils/User.java
+++ utils/User.java
@@ -1,3 +1,24 @@
 public class User {
 
+	private String usernameID;
+	private String mailDirection;
+	private String mailbox;
+	private String password;
+	
+	public User(String usernameID, String mailDirection, String mailbox, String password) {
+		this.sender = sender;
+		this.receiver = receiver;
+		this.text = text;
+		this.ID = ID;
+		this.password = password;
+	}
+	
+	public bool checkPassword(String password)
+	{
+		if(this.password==password){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
