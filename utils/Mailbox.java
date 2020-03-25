a5d6608e6c315a08be2e62a29a3791144a6affe6
Marutho <marutho97@gmail.com>
2020-03-25 13:52:36 +0100

    Javi
    Javi

--- utils/Mailbox.java
+++ utils/Mailbox.java
@@ -1,16 +1,29 @@
 
 public class Mailbox {
 
+	//Define the size of our Mailbox, final because we only declare it one time
+	private static final int MAILBOXSIZE = 999;			
+	//We create an array for the mails in this mailbox
+	private Email arrayMails[] = new Email[MAILBOXSIZE];
+	
 	public static void main(String[] args) {
-		
-		//Define the size of our Mailbox, final because we only declare it one time
-		private static final int MAILBOXSIZE = 0;
-		
-		//We create an array for the mails in this mailbox
-		Email arrayMails[] = new Email[MAILBOXSIZE];
+		System.out.println(stat());
+	    System.out.println(arrayMails[0]);	
 	} 
 	
 	public int stat() {
 		return arrayMails.length;
+	}
+	
+	public String list() {
+		
+	}
+	
+	public Email returnMailID() {
+		
+	}
+	
+	public void deleteID(String ID) {
+		
 	}
 }
